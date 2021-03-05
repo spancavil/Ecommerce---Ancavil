77eea4b100aa18b7c5d2d2b44d17899e50fca3c1
spancavil <soancavil@gmail.com>
2021-03-04 22:52:47 -0300

    Creación de componente (navbar)
    Se creó un componente navbar en la carpeta components, para luego utilizarlo en la app.

---
+++ src/components/navbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const NavBar = () => {
+  return (
+    <div>
+      <h3>El gatito azul</h3>
+      <ul>
+        <li>
+          <a href="">Rascador 1</a>
+        </li>
+        <li>
+          <a href="">Rascador 2</a>
+        </li>
+        <li>
+          <a href="">Esquinero</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
