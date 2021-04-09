68df97d4ee6dfc9d36f7ffbf0896d90d441690c4
spancavil <soancavil@gmail.com>
2021-04-08 22:51:33 -0300

    Agregado de modelo y modificado de página de contacto

--- src/assets/Items/index.js
+++ src/assets/Items/index.js
@@ -4,7 +4,8 @@ const Items = [
     {"id": 3, "stock": 7, "src": "/img/modelo3030dos.jpeg", "initial": 0, "description": "Modelo 3", "precio": 5800, "categoria": "double"},
     {"id": 4, "stock": 3, "src": "/img/modelo3050.jpeg", "initial": 0, "description": "Modelo 4", "precio": 7900, "categoria": "double"},
     {"id": 5, "stock": 2, "src": "/img/modelo3050dos.jpeg", "initial": 0, "description": "Modelo 5", "precio": 7900, "categoria":"double"},
-    {"id": 6, "stock": 4, "src": "/img/modeloMini.jpeg", "initial":0, "description": "Modelo 6", "precio": 1700, "categoria":"simple"}
+    {"id": 6, "stock": 4, "src": "/img/modeloMini.jpeg", "initial":0, "description": "Modelo 6", "precio": 1700, "categoria":"simple"},
+    {"id": 7, "stock": 3, "src": "/img/modelo2030.jpeg", "initial":0, "description": "Modelo 7", "precio": 2200, "categoria": "simple"}
     ]
 
 export default Items;
--- src/components/Presentacion/index.js
+++ src/components/Presentacion/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const Presentacion = () => {
     return(
+    <div className="text-center">
         <img src="/img/Portada2.jpg" className="img-fluid" alt="..."></img>
+    </div>
     )
 }
 
